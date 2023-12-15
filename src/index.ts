01749652b0ee66f12fac8213a9c8b7569725cd52
Alexander Khomenok <exleonardo@mail.ru>
2023-12-15 13:49:44 +0300

    fix:

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express()
 const port =process.env.PORT|| 5000
 const server = http.createServer(app)
 const socket = new Server(server,{cors: {
-        origin: "https://chatwebsocket-front.vercel.app/",
+        origin: "https://chatwebsocket-front.vercel.app",
         methods: ["GET", "POST"]
     }})
 
