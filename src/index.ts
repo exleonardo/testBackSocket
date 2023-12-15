56000f6d468dc26d40bbf64d51f6eab41de11cf6
Alexander Khomenok <exleonardo@mail.ru>
2023-12-15 15:35:16 +0300

    fix: error

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express()
 const port =process.env.PORT|| 5000
 const server = http.createServer(app)
 const socket = new Server(server,{cors: {
-        origin: false,
+        origin: '*',
         methods: ["GET", "POST"],
 
     }})
